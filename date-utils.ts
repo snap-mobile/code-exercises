815b086ad269f737acc1baf090faaee42412def4
Sam Raker <sam.raker@gmail.com>
2022-02-03 12:28:22 -0500

    [sam-roman]  our solution

--- date-utils.ts
+++ date-utils.ts
@@ -1,10 +1,5 @@
-
-const isWeekend = (inputDate): boolean => {
-  const day = inputDate.getDay();
-  if (day === 0 || day === 6) {
-    return true;
-  }
-  return false;
+const isWeekend = (inputDate: Date): boolean => {
+  return !(inputDate.getDay() % 6);
 };
 
 export default { isWeekend };
