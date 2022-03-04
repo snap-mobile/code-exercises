a03eb4bed41ad999e2f2ec62b5e93172801c044e
SlicoHP <rodrigojimenezgarcia87@gmail.com>
2022-03-04 21:45:57 +0100

    Jon & Rodrigo 1 cata ts

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,82 @@
-const fundraisers = [
+const fundraisers: Fundraiser[] = [
   {
     id: "1234",
     name: "Anytown Highschool Basketball",
-    startDate: new Date('December 17, 2021 03:24:00'),
+    startDate: new Date("December 17, 2021 03:24:00"),
     isActive: true,
     teamSize: 26,
     statusLog: [
-        {
-          date: new Date('December 15, 2021 01:01:01'),
-          status: "pending",
-        },
-        {
-          date: new Date('December 17, 2021 03:24:00'),
-          status: "active",
-        },
-        {
-          date: new Date('December 28, 2021 06:52:12'),
-          status: "closed",
-        }
-    ]
+      {
+        date: new Date("December 15, 2021 01:01:01"),
+        status: "pending",
+      },
+      {
+        date: new Date("December 17, 2021 03:24:00"),
+        status: "active",
+      },
+      {
+        date: new Date("December 28, 2021 06:52:12"),
+        status: "closed",
+      },
+    ],
   },
   {
     id: "5678",
     name: "Newtown Hockey Team",
-    startDate: new Date('January 23, 2022 03:24:00'),
+    startDate: new Date("January 23, 2022 03:24:00"),
     isActive: true,
     teamSize: 26,
     statusLog: [
-        {
-          date: new Date('January 12, 2022 01:01:01'),
-          status: "pending",
-        },
-        {
-          date: new Date('January 23, 2022 03:24:00'),
-          status: "active",
-        },
-        {
-          date: new Date('January 30, 2022 06:52:12'),
-          status: "closed",
-        }
-    ]
-  }
-]
+      {
+        date: new Date("January 12, 2022 01:01:01"),
+        status: "pending",
+      },
+      {
+        date: new Date("January 23, 2022 03:24:00"),
+        status: "active",
+      },
+      {
+        date: new Date("January 30, 2022 06:52:12"),
+        status: "closed",
+      },
+    ],
+  },
+];
 
-const lastStatusFor = (statusLog: any): any => {
-  return statusLog.reduce((a: any, b: any) => {
-    return a.date > b.date ? a : b;
-  });
+interface Fundraiser {
+  id: string;
+  name: string;
+  startDate: Date;
+  isActive: Boolean;
+  teamSize: number;
+  statusLog: StatusLog[];
 }
 
-const mostRecentStatuses = (fundraisers: any): any => {
-  return fundraisers.map((fundraiser: any): any => {
+type StatusLog = {
+  date: Date;
+  status: Status;
+};
+
+type Status = "active" | "closed" | "pending";
+
+type FundraiserData = {
+  fundId: string;
+  lastStatus: Status;
+};
+
+const lastStatusFor = (statusLog: StatusLog[]): StatusLog => {
+  return statusLog.reduce((a: StatusLog, b: StatusLog) => {
+    return a.date > b.date ? a : b;
+  });
+};
+
+const mostRecentStatuses = (fundraisers: Fundraiser[]): FundraiserData[] => {
+  return fundraisers.map((fundraiser: Fundraiser): FundraiserData => {
     return {
       fundId: fundraiser.id,
       lastStatus: lastStatusFor(fundraiser.statusLog).status,
-    }
-  })
-}
+    };
+  });
+};
 
 console.log(mostRecentStatuses(fundraisers));
