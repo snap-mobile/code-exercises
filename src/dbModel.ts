f2b6285743af6be87ddd5b4b959d53717583a48b
DC <dcnoye@gmail.com>
2022-06-02 12:45:54 -0400

    db to func

--- src/dbModel.ts
+++ src/dbModel.ts
@@ -1,16 +1,7 @@
-
-export default class DbModel {
-  config: string;
-
-  constructor(config: string) {
-    this.config = config;
-  }
-
-  where(params: any) {
-    console.log("Fetching from DB")
-    return { lastLogin: new Date }; // Mock DB Return
-  }
+export const dbinstance = (params: any, config: string) => {
+  console.log("Fetching from DB")
+  return { lastLogin: new Date }; 
+     // Mock DB Return
 }
-
 
 
