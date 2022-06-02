10611fb5aecc724ff8035a30a94d78ccd5c58548
Voityk Eugene <jackson0512@gmail.com>
2022-06-02 19:27:12 +0300

    import person from a separate file

--- src/person.ts
+++ src/person.ts
@@ -2,14 +2,7 @@
 import { generateToken } from './authToken';
 import { dbWhere } from './dbModel';
 import { sendEmail } from './emailer';
-
-export type Person = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  token?: string;
-  lastLogin?: Date;
-}
+import { Person } from "./iPerson";
 
 export const authenticate = (id: string, firstName: string, lastName: string): Person => {
   const person = generateToken({ id, firstName, lastName });
