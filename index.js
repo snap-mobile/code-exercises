164fb3d19888b5b72a407334d3f64f5d19ebbea4
James Foley <James.Foley@snapraise.com>
2022-10-20 10:30:39 -0700

    update setcookies and definition

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-// setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz', 'another-addition': 'added!'})
-//
-const setCookies = (allCookies, cookies = {}) => {
+// DEPRECATED: setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz', 'another-addition': 'added!'})
+
+// Function definition changed!!
+// setCookies(
+//   ['token-local', 'token-dev', 'token-staging', 'token'],
+//   ["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"],
+//   {'token-local': 'baz', 'another-addition': 'added!'})
+const setCookies = (cookieSuffix, allCookies, cookies = {}) => {
   try {
-    if (allCookies && cookies) {
+    if (allCookies && cookies && cookieSuffix.length > 0) {
       allCookies.forEach((cookie) => {
-        data = cookie.split(';').map((c) => {
-          return c.split("=")
-        }).reduce((result, element) => {
-          result[element[0].trim().toLowerCase()] = element[element.length - 1]
-          return result
-        }, {})
-        const cookieSuffix = ['token-local', 'token-dev', 'token-staging', 'token']
-        cookieSuffix.forEach((name) => {
-          if (data[name]) {
-            cookies[`${name}`] = {
-              value: data[name],
-              expires: data["expires"],
-              domain: data["domain"],
-              httponly: true,
-              secure: true
-            }
+        let name = ''
+        const data = cookie.split(';')
+        data.map((item, idx) => {
+          const values = item.split('=');
+          if (cookieSuffix.includes(values[0])) {
+            name = values[0]
+            cookies[name] = {value: values[1]}
+          } else {
+            cookies[name] = {...cookies[name],[values[0]]: values[1]}
+          }
+          if (idx === data.length - 1) {
+            cookies[name] = {...cookies[name], httponly: true, secure: true}
           }
         })
       })
-      return cookies
+        return cookies
     }
   } catch(err) {
     console.error(err);
