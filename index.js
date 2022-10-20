34d0dcc5145a863314df2f5fd0f7c49de1e43457
Voityk Eugene <jackson0512@gmail.com>
2022-10-20 20:17:33 +0300

    eugene ones

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-// setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'})
-//
-const setCookies = (allCookies, cookies = {}) => {
-  try {
-    if (allCookies && cookies) {
-      allCookies.forEach((cookie) => {
-        data = cookie.split(';').map((c) => {
-          return c.split("=")
-        }).reduce((result, element) => {
-          result[element[0].trim().toLowerCase()] = element[element.length - 1]
-          return result
-        }, {})
-        const cookieSuffix = ['token-local', 'token-dev', 'token-staging', 'token']
-        cookieSuffix.forEach((name) => {
-          if (data[name]) {
-            cookies[`${name}`] = {
-              value: data[name],
-              expires: data["expires"],
-              domain: data["domain"],
-              httponly: true,
-              secure: true
-            }
-          }
-        })
-      })
-      return cookies
+const setCookies2 = (allCookies, cookies = {}) => {
+  const whiteList = ['token-local', 'token-dev', 'token-staging', 'token'];
+  const result = {};
+
+  allCookies.forEach((cookie) => {
+    const array = cookie.split(';').map((x) => x.split('='))
+    const cookieName = array.shift()[0];
+    array.push(['value', cookieName]);
+    array.push(['httponly', true]);
+    array.push(['secure', true]);
+
+    if (whiteList.includes(cookieName)) {
+      const obj = Object.fromEntries(array.map(([k, v]) => [k, v]));
+      result[cookieName] = obj;
     }
-  } catch(err) {
-    console.error(err);
+  });
+
+  return { ...cookies, ...result };
+}
+
+const cookies = [
+  "token-local=foo;expires=20210101021234;domain=snap.app",
+  "token-dev=bar;expires=20210101021234;domain=snap.app"
+]
+const result = setCookies2(cookies, {'token-local2': 'baz'})
+console.log(result);
+
+/*
+{
+  'token-local2': 'baz',
+  'token-local': {
+    value: 'foo',
+    expires: '20210101021234',
+    domain: 'snap.app',
+    httponly: true,
+    secure: true
+  },
+  'token-dev': {
+    value: 'bar',
+    expires: '20210101021234',
+    domain: 'snap.app',
+    httponly: true,
+    secure: true
   }
 }
+*/
