8bae084eba191b0ad2a7800a70da26cd696f1f1d
Kyle Samuel <Ksamuel92@gmail.com>
2022-10-20 13:05:03 -0400

    to be continued

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
 // setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'})
 //
-const setCookies = (allCookies, cookies = {}) => {
+
+const processCookies = (cookies) => {
+  new Promise((resolve, reject) => {
+    const processedCookies = [];
+    cookies.forEach((cookie) => {
+      data = cookie
+        .split(';')
+        .map((c) => c.split('='))
+        .reduce((result, element) => {
+          result[element[0].trim().toLowerCase()] = element[element.length - 1];
+          processedCookies.push(result);
+        }, {});
+    });
+    resolve(processedCookies);
+  });
+};
+
+const cookieObj = (name, processedCookies, cookies) => {
+  new Promise((resolve, reject) => {
+    const result = processedCookies.map((cookie) => {
+      if (cookie[name]) {
+        cookies[`${name}`] = {
+          value: data[name],
+          expires: data['expires'],
+          domain: data['domain'],
+          httponly: true,
+          secure: true,
+        };
+      }
+      return cookie;
+    });
+    resolve(result);
+  });
+};
+
+const setCookies = async (allCookies, cookies = {}) => {
   try {
-    if (allCookies && cookies) {
-      allCookies.forEach((cookie) => {
-        data = cookie.split(';').map((c) => {
-          return c.split("=")
-        }).reduce((result, element) => {
-          result[element[0].trim().toLowerCase()] = element[element.length - 1]
-          return result
-        }, {})
-        const cookieSuffix = ['token-local', 'token-dev', 'token-staging', 'token']
-        cookieSuffix.forEach((name) => {
-          if (data[name]) {
-            cookies[`${name}`] = {
-              value: data[name],
-              expires: data["expires"],
-              domain: data["domain"],
-              httponly: true,
-              secure: true
-            }
-          }
-        })
-      })
-      return cookies
-    }
-  } catch(err) {
+    if (!(allCookies && cookies))
+      throw new Error('You must provide both arguments');
+    processCookies(allCookies).then((data) => console.log(data));
+
+    return Promise.all([
+      await cookieObj('token-local', processedCookies, cookies),
+      await cookieObj('token-dev', processedCookies, cookies),
+      await cookieObj('token-staging', processedCookies, cookies),
+      await cookieObj('token', processedCookies, cookies),
+    ]);
+  } catch (err) {
     console.error(err);
   }
-}
+};
+
+result = setCookies(
+  [
+    'token-local=foo;expires=20210101021234;domain=snap.app',
+    'token-dev=bar;expires=20210101021234;domain=snap.app',
+  ],
+  { 'token-local': 'baz' }
+);
+
