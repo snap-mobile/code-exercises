6e04e830c747ffb1f6ef2a1e739c989116975103
jed <johncd@codev.com>
2022-10-21 01:15:14 +0800

    just a try

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
 // setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'})
 //
 const setCookies = (allCookies, cookies = {}) => {
-  try {
-    if (allCookies && cookies) {
-      allCookies.forEach((cookie) => {
-        data = cookie.split(';').map((c) => {
-          return c.split("=")
-        }).reduce((result, element) => {
-          result[element[0].trim().toLowerCase()] = element[element.length - 1]
-          return result
-        }, {})
-        const cookieSuffix = ['token-local', 'token-dev', 'token-staging', 'token']
-        cookieSuffix.forEach((name) => {
-          if (data[name]) {
-            cookies[`${name}`] = {
-              value: data[name],
-              expires: data["expires"],
-              domain: data["domain"],
-              httponly: true,
-              secure: true
-            }
-          }
-        })
-      })
-      return cookies
-    }
-  } catch(err) {
-    console.error(err);
-  }
-}
-
-
-const setCookiesNew = (allCookies, cookies = {}) => {
   if (!allCookies.length) {
     return cookies;
   }
-  allCookies.forEach((cookie) => {
+  return allCookies.map((cookie) => {
     data = cookie.split(';').map((c) => {
       return c.split("=");
     });
@@ -44,18 +13,12 @@ const setCookiesNew = (allCookies, cookies = {}) => {
       obj[`${prop[0]}`.trim().toLowerCase()] = prop[1];
       return obj;
     });
-    console.log(props);
-    // return props.map(d => {
-    //   return d.reduce((acc, cur) => {
-    //     return {
-    //       ...acc,
-    //       ...cur,
-    //     }
-    //   }, {});
-    // })
+    return props.reduce((acc, curr) => {
+      return {...acc, ...curr};
+    }, {});
   });
 };
 
-const x = setCookiesNew(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'});
+const x = setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'})
 
 console.log(x);
