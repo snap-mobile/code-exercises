c578f7e6910c5d2a852c9e58b799d226de87ef59
Willie Wrinkle <William.Wrinkle@snapraise.com>
2022-10-20 12:21:27 -0500

    Meaningful commit message

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-// setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'})
-//
 const setCookies = (allCookies, cookies = {}) => {
   try {
+    const suffixes = ["token-local", "token-dev", "token"];
     if (allCookies && cookies) {
-      allCookies.forEach((cookie) => {
-        data = cookie.split(';').map((c) => {
-          return c.split("=")
-        }).reduce((result, element) => {
-          result[element[0].trim().toLowerCase()] = element[element.length - 1]
-          return result
-        }, {})
-        const cookieSuffix = ['token-local', 'token-dev', 'token-staging', 'token']
-        cookieSuffix.forEach((name) => {
-          if (data[name]) {
-            cookies[`${name}`] = {
-              value: data[name],
-              expires: data["expires"],
-              domain: data["domain"],
-              httponly: true,
-              secure: true
-            }
+      allCookies.forEach((cookieString) => {
+        let name;
+        const cookie = {};
+        const data = cookieString.split(";");
+        data.forEach((item, idx) => {
+          const [key, value] = item.split("=");
+          if (suffixes.includes(key)) {
+            name = key;
+            cookie.value = value;
+          } else {
+            cookie[key] = value;
           }
-        })
-      })
-      return cookies
+        });
+        console.log("name cookie", name, cookie);
+        if (name) {
+          cookies[name] = cookie;
+        }
+      });
+      return cookies;
     }
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   }
-}
+};
+
+console.log(
+  setCookies(
+    [
+      "token-local=foo;expires=20210101021234;domain=snap.app",
+      "token-dev=bar;expires=20210101021234;domain=snap.app",
+    ],
+    { "token-local": "baz", "another-addition": "added!" }
+  )
+);
