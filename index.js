61e8b14dda90e4269897ae6083e2a7610792125c
Roman Rahman <rahman.roman@gmail.com>
2022-10-20 20:01:28 +0300

    More less code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz', 'another-addition': 'added!'})
 //
 const setCookies =  (allCookies, cookies = {}) => {
-  return {...cookies, ...['token-local', 'token-dev', 'token-staging', 'token'].reduce((acc, cookieName) => {
-      const data = allCookies.map(cookie => (Object.fromEntries(cookie.split(';').map(c => c.split('=')).map(([name, value]) => ([name.trim().toLowerCase(), value]))))).find(c => !!c[cookieName]);
-      const update = data ? {[cookieName]: {value: data[cookieName],expires: data["expires"],domain: data["domain"],httponly: true,secure: true}} : {};
-      return {...acc, ...update};
+    return {...cookies, ...['token-local', 'token-dev', 'token-staging', 'token'].reduce((acc, cookieName) => {
+        const data = allCookies.map(cookie => (Object.fromEntries(cookie.split(';').map(c => c.split('=')).map(([name, value]) => ([name.trim().toLowerCase(), value]))))).find(c => !!c[cookieName]);
+        return {...acc, ...data ? {[cookieName]: {value: data[cookieName],expires: data["expires"],domain: data["domain"],httponly: true,secure: true}} : {}};
     }, {})};
-}
+};
