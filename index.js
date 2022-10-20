6745282cab21faa9d54243e1ae481b1ea25e3d0a
jed <johncd@codev.com>
2022-10-21 01:04:43 +0800

    not quite finished

--- index.js
+++ index.js
@@ -29,3 +29,33 @@ const setCookies = (allCookies, cookies = {}) => {
     console.error(err);
   }
 }
+
+
+const setCookiesNew = (allCookies, cookies = {}) => {
+  if (!allCookies.length) {
+    return cookies;
+  }
+  allCookies.forEach((cookie) => {
+    data = cookie.split(';').map((c) => {
+      return c.split("=");
+    });
+    props = data.map(prop => {
+      let obj = {};
+      obj[`${prop[0]}`.trim().toLowerCase()] = prop[1];
+      return obj;
+    });
+    console.log(props);
+    // return props.map(d => {
+    //   return d.reduce((acc, cur) => {
+    //     return {
+    //       ...acc,
+    //       ...cur,
+    //     }
+    //   }, {});
+    // })
+  });
+};
+
+const x = setCookiesNew(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'});
+
+console.log(x);
