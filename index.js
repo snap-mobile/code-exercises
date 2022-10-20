a6aeb73f377d2fecaef283880b5f3161586f5842
Loumonth <loumonth.jack.jr@gmail.com>
2022-10-20 11:29:11 -0500

    29->24 lines of code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // setCookies(["token-local=foo;expires=20210101021234;domain=snap.app","token-dev=bar;expires=20210101021234;domain=snap.app"], {'token-local': 'baz'})
-//
+
+// 29 lines of code
 const setCookies = (allCookies, cookies = {}) => {
   try {
     if (allCookies && cookies) {
@@ -25,7 +26,39 @@ const setCookies = (allCookies, cookies = {}) => {
       })
       return cookies
     }
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   }
 }
+
+// 24 lines of code
+const setNewCookies = (allCookies, cookies = {}) => {
+  if (!allCookies) return cookies;
+  if (!Array.isArray(allCookies)) return cookies;
+  allCookies.forEach((cookie) => {
+    const cookieData = cookie.split(';').map((c) => {
+      return c.split("=")
+    }).reduce((result, element) => {
+      result[element[0].trim().toLowerCase()] = element[element.length - 1]
+      return result
+    }, {});
+
+    ['token-local', 'token-dev', 'token-staging', 'token'].forEach((name) => {
+      if (!cookieData[name]) return;
+      cookies[`${name}`] = {
+        value: cookieData[name],
+        expires: cookieData["expires"],
+        domain: cookieData["domain"],
+        httponly: true,
+        secure: true
+      }
+    })
+  })
+  return cookies
+} 
+
+const cookies = setNewCookies(["token-local=foo;expires=20210101021234;domain=snap.app", "token-dev=bar;expires=20210101021234;domain=snap.app"], { 'token-local': 'baz' })
+console.log('Old Cookie',cookies)
+
+const newCookies = setCookies(["token-local=foo;expires=20210101021234;domain=snap.app", "token-dev=bar;expires=20210101021234;domain=snap.app"], { 'token-local': 'baz' })
+console.log('New Cookie', newCookies)
